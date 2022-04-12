0092dcd7508ee1864c8cc2d756a51c2b5182f287
Dante Lopez <lopezzdante17@gmail.com>
2022-04-12 15:09:24 -0300

    TP2-28: creada pantalla de alta de cliente

---
+++ Front/Js/validacionLogin.js
@@ -0,0 +1,13 @@
+function validar() {
+  let exprReg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+  let correo = $("#txtEmail");
+  let clave = $("#txtClave");
+  if (correo.val() == "" || !exprReg.test(correo.val())) {
+    swal("Error", "ingrese un email valido", "error");
+    correo.focus();
+    return false;
+  }
+  if(clave.val() == ""){
+    swal("Error", "ingrese una contraseña", "error");
+  }
+}
